179d9ed199ebdbb6dff105a1551e0bf2017260a9
Margaret Schaub <margaretcschaub@outlook.com>
2024-03-01 20:01:22 -0700

    cleaned up

--- GDC_downlaod.R
+++ GDC_downlaod.R
@@ -3,6 +3,7 @@ library(tidyverse)
 library(maftools)
 library(pheatmap)
 library(SummarizedExperiment)
+library(dplyr)
 
 gdcprojects <- getGDCprojects()
 getProjectSummary('TCGA-OV')
@@ -22,21 +23,8 @@ GDCdownload(query_TCGA)
 tcga_ov_data <- GDCprepare(query_TCGA, summarizedExperiment = TRUE)
 ov_matrix <- assay(tcga_ov_data, 'unstranded')
 
-query_TCGA_test <- GDCquery(project = 'TCGA-OV',
-                            data.category = 'Transcriptome Profiling',
-                            access = 'open',
-                            experimental.strategy = 'RNA-Seq',
-                            barcode= c('TCGA-24-1104', 
-                                       'TCGA-25-1320', 
-                                       'TCGA-24-1850'))
 
-output_query_test <- getResults(query_TCGA_test)
-GDCdownload(query_TCGA_test)
-
-tcga_ov_data_short <- GDCprepare(query_TCGA_test, summarizedExperiment = TRUE)
-ov_matrix_short <- assay(tcga_ov_data_short, 'unstranded')
-
-clinical_a <- GDCquery_clinic(project = "TCGA-OV", type = "clinical")
+# clinical_a <- GDCquery_clinic(project = "TCGA-OV", type = "clinical")
 
 clin.query <- GDCquery(project = "TCGA-OV", data.category = "Clinical",
                        data.format = 'BCR XML',
@@ -44,19 +32,11 @@ clin.query <- GDCquery(project = "TCGA-OV", data.category = "Clinical",
                   'TCGA-25-1320', 
                   'TCGA-24-1850'))
 
-clin_output <- getResults(clin.query)
-
-
 GDCdownload(clin.query)
 
 clinical <- GDCprepare_clinic(clin.query,"patient")
 
-
-library(dplyr)
-
-
-clinical_x <- mutate(
+clinical_new_col <- mutate(
   clinical, new_age = ifelse(age_at_initial_pathologic_diagnosis<50, 0, 1))
 
-class(clinical_x)
-head(clinical_x)
+head(clinical_new_col)
