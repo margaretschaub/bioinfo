937095179cb70d50a142f9478acc762b64e17734
Margaret Schaub <margaretcschaub@outlook.com>
2024-03-08 16:16:26 -0700

    full TCGA OV data

--- GDC_downlaod.R
+++ GDC_downlaod.R
@@ -12,25 +12,38 @@ query_TCGA <- GDCquery(project = 'TCGA-OV',
          data.category = 'Transcriptome Profiling',
          access = 'open',
          experimental.strategy = 'RNA-Seq',
-         barcode= c('TCGA-24-1104-01A-01R-1565-13', 
-                    'TCGA-25-1320-01A-01R-1565-13', 
-                    'TCGA-24-1850-01A-01R-1567-13'))
+         sample.type = 'Primary Tumor')
 
 output_query <- getResults(query_TCGA)
+
+
+#testing for technical variants
+cases <- output_query$cases
+cases_list <- as.list(cases)
+is_unique <- length(cases_list) == length(unique(cases_list))
+print(is_unique)
+
+extract_first_12 <- function(x) {
+  substr(x, 1, 12)
+}
+
+
+modified_list <- lapply(cases_list, extract_first_12)
 
 GDCdownload(query_TCGA)
 
 tcga_ov_data <- GDCprepare(query_TCGA, summarizedExperiment = TRUE)
+
+
 ov_matrix <- assay(tcga_ov_data, 'unstranded')
 
+colnames(ov_matrix) <- substr(colnames(ov_matrix), 1, 12)
 
 # clinical_a <- GDCquery_clinic(project = "TCGA-OV", type = "clinical")
 
 clin.query <- GDCquery(project = "TCGA-OV", data.category = "Clinical",
                        data.format = 'BCR XML',
-        barcode= c('TCGA-24-1104', 
-                  'TCGA-25-1320', 
-                  'TCGA-24-1850'))
+        barcode= c(modified_list))
 
 GDCdownload(clin.query)
 
@@ -40,3 +53,11 @@ clinical_new_col <- mutate(
   clinical, new_age = ifelse(age_at_initial_pathologic_diagnosis<50, 0, 1))
 
 head(clinical_new_col)
+
+write.csv(clinical_new_col, 
+          '/Users/margaretschaub/Bioinformatics Open Source Project/new_col.csv'
+          ,row.names = FALSE)
+write.csv(ov_matrix, 
+          '/Users/margaretschaub/Bioinformatics Open Source Project/ov.csv' 
+          ,row.names = FALSE)
+
--- deseq2_test.R
+++ deseq2_test.R
@@ -23,29 +23,40 @@ all(colnames(ov_matrix) == rownames(colData))
 
 # if order doesn't match 
 
-barcode_order_matches <- identical(colnames(ov_matrix), barcode_column)
-
 barcode_order <- colnames(ov_matrix)
-ov_matrix <- ov_matrix[, match(barcode_order, barcode_column)]
-barcode_order_matches <- identical(colnames(ov_matrix), barcode_column)
+
+new_colData <- colData[match(barcode_order, colData$bcr_patient_barcode),]
+
+all(colnames(ov_matrix) == rownames(new_colData))
 
 # creating deseq object
+new_colData$new_age <- factor(new_colData$new_age)
 dds <- DESeqDataSetFromMatrix(countData= ov_matrix,
-                       colData = colData,
+                       colData = new_colData,
                        design = ~ new_age)
-# right now, only 3 samples to keep download size low. 
-# design factor does not have multiple variables with current samples
+
+dds
 
 keep <- rowSums(counts(dds)) >= 10
-dds<- dds[Keep,]
+dds<- dds[keep,]
+
+dds
 
 # should reference level be younger or older than 50 years? 
 dds$new_age <- relevel(dds$new_age, ref= '0')
 
-# will not currently run due to lack of data, samples do not have diff design factor
+dds$new_age
+
+plotMDS(dds)
+
 dds <- DESeq(dds)
+
 res <- results(dds)
 
-# are there any tech replicates that need to be collapsed? 
+res
 
+summary(res)
 
+res0.01 <- results(dds, alpha = 0.01)
+
+summary(res0.01)
