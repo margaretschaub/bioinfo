90b1fcde5c9dc646de245e3ccb27239804734436
Margaret Schaub <margaretcschaub@outlook.com>
2024-03-23 21:10:50 -0600

    plot

--- deseq2_test.R
+++ deseq2_test.R
@@ -1,10 +1,13 @@
 BiocManager::install("DESeq2")
 BiocManager::install("airway")
+BiocManager::install("Glimma")
 library(pheatmap)
 library(DESeq2)
 library(tidyverse)
 library(airway)
 library(dplyr)
+library('Glimma')
+
 
 patients_ov_matrix <- colnames(ov_matrix)
 print(patients_ov_matrix)
@@ -55,6 +58,7 @@ summary(res)
 res0.01 <- results(dds, alpha = 0.01)
 
 summary(res0.01)
+print(res0.01)
 
 print(length(rownames(res0.01)))
 
@@ -64,6 +68,7 @@ print(head(res0.01))
 
 gene_codes <- rownames(res0.01)
 gene_names <- gene_metadata$gene_name[match(gene_codes, gene_metadata$gene_id)]
+res$symbol <- gene_names
 
 rownames(res0.01) <- gene_names
 summary(res0.01)      
@@ -80,8 +85,39 @@ significant_genes <- subset(res0.01, padj < 0.01)
 print(rownames(significant_genes))
 
 fold_change_threshold <- 1.5  # Adjust as needed
-large_fold_change_genes <- subset(res0.01, abs(log2FoldChange) > fold_change_threshold)
+large_fold_change_genes <- subset(res, abs(log2FoldChange) > fold_change_threshold)
 print(rownames(large_fold_change_genes))
-important_genes <- subset(res0.01, padj < 0.05 & abs(log2FoldChange) > fold_change_threshold)
+important_genes <- subset(res, padj < 0.01 & abs(log2FoldChange) > fold_change_threshold)
 print(rownames(important_genes))
+print(important_genes)
 
+# Create interactive report with Glimma
+
+status <- as.numeric(res0.01$padj < .01)
+anno <- data.frame(GeneID=rownames(res0.01))
+tmp <- '/Users/margaretschaub/Desktop'
+glMDPlot(res0.01, status=status, counts=counts(dds,normalized=TRUE),
+         groups=dds$meno_status, transform=FALSE,
+         samples=colnames(dds), anno=anno,
+         path=tmp, folder="glimma", launch=FALSE)
+
+browseURL(file.path(tmp,"glimma","MD-Plot.html"))
+
+# Get data for specific genes of interest identified from plots
+row_index <- which(rownames(res0.01) == 'IGKC')
+result_for_gene <- res0.01[row_index, ]
+print(result_for_gene)
+
+
+# Alternate glimma with gene ID and symbol
+status <- as.numeric(res$padj < .01)
+anno <- data.frame(GeneID=rownames(res), symbol=res$symbol)
+glMDPlot(res, status=status, counts=counts(dds,normalized=TRUE),
+         groups=dds$meno_status, transform=FALSE,
+         samples=colnames(dds), anno=anno,
+         path=tmp, folder="glimma", launch=FALSE)
+
+browseURL(file.path(tmp,"glimma","MD-Plot.html"))
+
+plotMA(important_genes)
+glimmaMA(important_genes)
